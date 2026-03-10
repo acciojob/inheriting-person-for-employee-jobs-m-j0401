b17027dab8abb8174ca534583240e5fb4e20e4f2
m-j0401 <mehul.jain2206@gmail.com>
2026-03-10 15:54:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ class Person {
 	}
 	greet()
 	{
-		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
+		console.log(`Hello, my name is ${this.name} I am ${this.age} years old.`)
 	}
 } 
 
@@ -20,6 +20,7 @@ class Employee extends Person
 	{
 		//console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`)
 		console.log(`Hello, my name is Alice and I am 25 years old.`)
+		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`)
 	}
 	}
 
